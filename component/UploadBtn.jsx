d859e8e3d485a849327ce240659ce7ca48946e93
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-11-24 20:53:17 -0500

    add upload btn

--- component/UploadBtn.jsx
+++ component/UploadBtn.jsx
@@ -1,2 +1,18 @@
 import React from "react";
+import { Button, StyleSheet, Text, View } from 'react-native';
 
+export default function UploadBtn(){
+  return(
+    <View style={styles.container}>
+      <Button title="Upload Image" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
