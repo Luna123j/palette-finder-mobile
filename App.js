db43197b659661e5092672502e1790788d4b8180
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-11-25 13:04:55 -0500

    try image component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ImageArea from './component/ImageArea';
 import UploadBtn from './component/UploadBtn';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <ImageArea />
       <UploadBtn />
       <StatusBar style="auto"/>
     </View>
--- component/ImageArea.jsx
+++ component/ImageArea.jsx
@@ -5,7 +5,7 @@ import { Button, Image, StyleSheet, Text, View } from 'react-native';
 export default function ImageArea(){
   return(
     <View>
-      <Image></Image>
+      <Image source = {{uri:'https://www.seekpng.com/png/detail/109-1095120_art-ghost-by-slhqueenbee-pixel-art-ghost.png'}} style = {{ width: 200, height: 200 }}/>
     </View>
   )
 }
