fd86a3ad4026dbe9c7b309a7ba541e42b36a879e
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-11-24 21:29:19 -0500

    styling upload button

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import UploadBtn from './component/UploadBtn';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <UploadBtn />
       <StatusBar style="auto"/>
     </View>
   );
--- component/UploadBtn.jsx
+++ component/UploadBtn.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function UploadBtn(){
   return(
     <View style={styles.container}>
-      <Button title="Upload Image" />
+      <Pressable style ={styles.btn} >
+        <Text style={styles.text}>Upload Image to Start</Text>
+      </Pressable>
     </View>
   )
 }
@@ -14,5 +16,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+
+  btn:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text:{
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   }
 })
